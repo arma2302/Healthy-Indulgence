46da82f38b1f74c93b63b78a21acf1a9bc3f12e4
arma2302 <126476531+arma2302@users.noreply.github.com>
2023-03-13 15:53:22 +0530

    header changed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,14 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos || currentScrollPos <= 360) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
 var swiper = new Swiper(".swiperbanner", {
   effect: "fade",
   fadeEffect: { crossFade: true },
